4129d9c8534ee98bd0cb5b2f5c810a892ed8cfaf
Andy Strube <andystrube64@gmail.com>
2024-07-29 23:06:14 -0700

    Added some styling for the home page

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -2,18 +2,18 @@ import { Link } from 'react-router-dom';
 
 function Root(props) {
   if(props.loggedInUser) {
-    return <div className="green">
+    return <div className="green box">
       <h2>
-        <div onClick={() => props.setLoggedInUser(null)}>Log out</div>
-        <div><Link to={'about'}>About page</Link></div>
+        <div className="log-out-button" onClick={() => props.setLoggedInUser(null)}>Log out</div>
+        <div className="link-spacing"><Link className="green" to={'about'}>About page</Link></div>
       </h2>
     </div>
   } else {
-    return <div className="green">
+    return <div className="green box">
       <h2>
-        <div><Link to={'sign-up'}>Sign Up</Link></div>
-        <div><Link to={'log-in'}>Log In</Link></div>
-        <div><Link to={'about'}>About page</Link></div>
+        <div className="link-spacing"><Link className="green" to={'sign-up'}>Sign Up</Link></div>
+        <div className="link-spacing"><Link className="green" to={'log-in'}>Log In</Link></div>
+        <div className="link-spacing"><Link className="green" to={'about'}>About page</Link></div>
       </h2>
     </div>
   }
