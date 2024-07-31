03b3c5a1958619b961002276fd3a74c9605cc47e
Andy Strube <andystrube64@gmail.com>
2024-07-30 17:36:33 -0700

    Small styling changes for the about page

--- src/routes/about.jsx
+++ src/routes/about.jsx
@@ -1,11 +1,10 @@
-import { Link } from 'react-router-dom';
-
 function AboutPage() {
-  return <h2 className="green">
-    Hello and welcome to my hacking minigame project thats inspired from the
-    one in the Fallout games!
-    <div><Link to={'/'}>Home page</Link></div>
-  </h2>  
+  return <div className="green page-content">
+    <h2>
+      Hello and welcome to my hacking minigame project thats inspired from the
+      one in the Fallout games!
+    </h2>
+  </div>
 }
 
-export default AboutPage
+export default AboutPage;
