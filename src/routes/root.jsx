922c11d20802169adecab360ff7aaf6d9b1d99c5
Andy Strube <andystrube64@gmail.com>
2024-08-07 14:16:11 -0700

    Goodbye login / sign up forms, hello auth0!

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,22 +1,21 @@
 import { Link } from 'react-router-dom';
+import LoginButton from '../misc/LoginButton';
+import LogoutButton from '../misc/LogoutButton';
 
 function Root(props) {
-  if(props.loggedInUser) {
-    return <div className="green box">
-      <h2>
-        <div className="log-out-button" onClick={() => props.setLoggedInUser(null)}>Log out</div>
-        <div className="link-spacing"><Link className="green" to={'about'}>About page</Link></div>
-      </h2>
-    </div>
-  } else {
-    return <div className="green box">
-      <h2>
-        <div className="link-spacing"><Link className="green" to={'sign-up'}>Sign Up</Link></div>
-        <div className="link-spacing"><Link className="green" to={'log-in'}>Log In</Link></div>
-        <div className="link-spacing"><Link className="green" to={'about'}>About page</Link></div>
-      </h2>
-    </div>
+
+  function handleUserFlowButton() {
+    if(props.loggedInUser) {
+      return <LogoutButton/>;
+    } else {
+      return <LoginButton/>;
+    }
   }
+
+  return <div className="green box">
+    {handleUserFlowButton()}
+    <h2 className="link-spacing"><Link className="green" to={'about'}>About page</Link></h2>
+  </div>
 }
 
 export default Root;
